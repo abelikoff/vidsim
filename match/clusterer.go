5467201b59e3553e1a93ed4554f9e8c5ae1ef181
Alexander L. Belikoff <alexander@belikoff.net>
2026-01-11 15:29:03 -0500

    Reimplemented strict clustering logic, fixing several bugs.

--- match/clusterer.go
+++ match/clusterer.go
@@ -13,6 +13,8 @@
 package match
 
 import (
+	"sort"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -44,10 +46,9 @@ func NewClusterer(method ClusteringMethod, logger *logrus.Logger) Clusterer {
 		}
 	case Strict:
 		return &StrictClusterer{
-			allMatches:  make(map[int]map[int]struct{}),
-			groups:      make(map[int][]int),
-			nextGroupID: 1,
-			logger:      logger,
+			allMatches: make(map[int]map[int]struct{}),
+			clusters:   make(map[int][]int),
+			logger:     logger,
 		}
 	case Loose:
 		return &UnionClusterer{
@@ -125,10 +126,9 @@ func (clr *TrivialClusterer) DebugDump() {
 // Strict clusterer - only combine elements into a group if all elements match pairwise
 
 type StrictClusterer struct {
-	allMatches  map[int]map[int]struct{} // All matched pairs (encoded by matches[X][Y] being present).
-	groups      map[int][]int            // group ID -> list of frame IDs
-	nextGroupID int                      // next group ID to assign
-	logger      *logrus.Logger
+	allMatches map[int]map[int]struct{} // All matched pairs (encoded by matches[X][Y] being present).
+	clusters   map[int][]int            // group ID -> list of frame IDs
+	logger     *logrus.Logger
 }
 
 // Add a pair of matched frames
@@ -153,41 +153,59 @@ func (clr *StrictClusterer) AddMatch(frameID1 int, frameID2 int) {
 	}
 
 	clr.allMatches[frameID1][frameID2] = struct{}{}
+}
 
-	// Incorporate the pair into each group that fully matches both frames
+// Cluster collected pair-wise matches into groups where
+// each element matches all other elements in the group.
 
-	foundMatches := false
+func (clr *StrictClusterer) clusterMatches() {
+	clr.clusters = make(map[int][]int)
+	nextGroupID := 1
 
-	for groupID, group := range clr.groups {
-		clr.logger.Debugf("checking if both %d and %d match group %d  (%v)\n", frameID1, frameID2, groupID, group)
+	for frameID1, frameIDs := range clr.allMatches {
+		for frameID2 := range frameIDs {
+			clr.logger.Debugf("clustering match for pair %d, %d\n", frameID1, frameID2)
 
-		if clr.frameMatchesGroup(frameID1, groupID) && clr.frameMatchesGroup(frameID2, groupID) {
-			clr.groups[groupID] = removeDuplicates(append(group, frameID1, frameID2))
-			clr.logger.Debugf("GROUP MATCH: added both %d and %d to group %d  ==>  (%v)\n",
-				frameID1, frameID2, groupID, clr.groups[groupID])
-			foundMatches = true
+			// Check if the pair matches all elements in any of the existing groups
+
+			foundMatches := false
+
+			for groupID, group := range clr.clusters {
+				clr.logger.Debugf("checking if both %d and %d match group %d  (%v)\n", frameID1, frameID2, groupID, group)
+
+				if clr.frameMatchesGroup(frameID1, groupID) && clr.frameMatchesGroup(frameID2, groupID) {
+					clr.clusters[groupID] = normalizeGroup(append(group, frameID1, frameID2))
+					clr.logger.Debugf("GROUP MATCH: added both %d and %d to group %d  ==>  (%v)\n",
+						frameID1, frameID2, groupID, clr.clusters[groupID])
+					foundMatches = true
+				}
+			}
+
+			// If the pair didn't match any existing cluster, create a new one.
+
+			if !foundMatches {
+				clr.clusters[nextGroupID] = []int{frameID1, frameID2}
+				clr.logger.Debugf("created new group for %d and %d ==> group %d (%v)\n",
+					frameID1, frameID2, nextGroupID, clr.clusters[nextGroupID])
+				nextGroupID++
+			}
 		}
 	}
 
-	if foundMatches {
-		return
-	}
-
-	// no existing group matches - create a new group
-	clr.groups[clr.nextGroupID] = []int{frameID1, frameID2}
-	clr.logger.Debugf("created new group for %d and %d ==> group %d (%v)\n",
-		frameID1, frameID2, clr.nextGroupID, clr.groups[clr.nextGroupID])
-	clr.nextGroupID++
+	clr.removeDuplicateClusters()
 }
 
 // Return the list of created groups
 
 func (clr *StrictClusterer) Groups() [][]int {
-	groups := make([][]int, 0, len(clr.groups))
+	clr.clusterMatches()
+	groups := make([][]int, 0, len(clr.clusters))
 
-	for _, group := range clr.groups {
+	for _, group := range clr.clusters {
 		groups = append(groups, group)
 	}
+
+	clr.logger.Debugf("Returning %d groups: %v\n", len(groups), groups)
 
 	return groups
 }
@@ -206,7 +224,7 @@ func (clr *StrictClusterer) DebugDump() {
 
 	clr.logger.Debug("--- groups ---")
 
-	for groupID, group := range clr.groups {
+	for groupID, group := range clr.clusters {
 		clr.logger.Debugf("group %d:  %v\n", groupID, group)
 	}
 
@@ -234,18 +252,22 @@ func (clr *StrictClusterer) match(frameID1 int, frameID2 int) bool {
 // Check if a frame matches a group
 
 func (clr *StrictClusterer) frameMatchesGroup(frameID int, groupID int) bool {
-	if group, found := clr.groups[groupID]; found {
+	if group, found := clr.clusters[groupID]; found {
 		for _, groupFrameID := range group {
 			if !clr.match(frameID, groupFrameID) {
+				clr.logger.Debugf("frame %d DID NOT match group %d\n", frameID, groupID)
 				return false
 			}
 		}
 	}
 
+	clr.logger.Debugf("frame %d MATCHED group %d\n", frameID, groupID)
 	return true
 }
 
-func removeDuplicates(array []int) []int {
+// sort IDs in the group and remove duplicates
+
+func normalizeGroup(array []int) []int {
 	seen := make(map[int]bool)
 	result := []int{}
 
@@ -256,7 +278,54 @@ func removeDuplicates(array []int) []int {
 		}
 	}
 
+	sort.Ints(result)
 	return result
+}
+
+// Remove duplicate groups (identical sets of members)
+
+func (clr *StrictClusterer) removeDuplicateClusters() {
+	groupIDs := make([]int, 0, len(clr.clusters))
+
+	for gid := range clr.clusters {
+		groupIDs = append(groupIDs, gid)
+	}
+
+	sort.Ints(groupIDs)
+	seenSets := make([]map[int]struct{}, 0, len(groupIDs))
+
+	for _, gid := range groupIDs {
+		group := clr.clusters[gid]
+		set := make(map[int]struct{}, len(group))
+		for _, id := range group {
+			set[id] = struct{}{}
+		}
+
+		dup := false
+		for _, s := range seenSets {
+			if len(s) != len(set) {
+				continue
+			}
+			equal := true
+			for k := range set {
+				if _, ok := s[k]; !ok {
+					equal = false
+					break
+				}
+			}
+			if equal {
+				dup = true
+				break
+			}
+		}
+
+		if dup {
+			clr.logger.Debugf("removing duplicate group %d => %v\n", gid, group)
+			delete(clr.clusters, gid)
+		} else {
+			seenSets = append(seenSets, set)
+		}
+	}
 }
 
 // =====================================================================================
@@ -330,7 +399,7 @@ func (clr *UnionClusterer) AddMatch(frameID1 int, frameID2 int) {
 	if clr.groups[resultingBucket] == nil {
 		clr.groups[resultingBucket] = []int{frameID1, frameID2}
 	} else {
-		clr.groups[resultingBucket] = removeDuplicates(append(clr.groups[resultingBucket], frameID1, frameID2))
+		clr.groups[resultingBucket] = normalizeGroup(append(clr.groups[resultingBucket], frameID1, frameID2))
 	}
 }
 
